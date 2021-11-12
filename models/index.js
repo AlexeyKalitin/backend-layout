d6df23a4c363c8167de9a26f99371c63fd8245d2
Alex <lksjkltn@gmail.com>
2021-11-12 16:44:38 +0300

    make it better

--- models/index.js
+++ models/index.js
@@ -1,37 +1,49 @@
-'use strict';
+'use strict'
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
+const fs = require('fs')
+const path = require('path')
+const Sequelize = require('sequelize')
+const basename = path.basename(__filename)
+const env = process.env.NODE_ENV || 'development'
+const config = require(__dirname + '/../config/config.json')[env]
+const db = {}
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = new Sequelize('dbsg14drdr6a9f', 'dgvtwskyrqrcgs', '3b103119070b6ea8cc243a66ad6853ae34bc8e7c437a2e1f794675d68657aeea',{
+  host: 'ec2-54-220-243-77.eu-west-1.compute.amazonaws.com',
+  dialect: 'postgres',
+  ssl: true,
+  protocol: "postgres",
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
+  logging: true,
+  dialectOptions: {
+      ssl: {
+          require: true,
+          rejectUnauthorized: false // <<<<<< YOU NEED THIS
+      }
   }
 });
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+fs.readdirSync(__dirname)
+  .filter(file => {
+    return (
+      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+    )
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    )
+    db[model.name] = model
+  })
 
-module.exports = db;
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db)
+  }
+})
+
+db.sequelize = sequelize
+db.Sequelize = Sequelize
+
+module.exports = db
