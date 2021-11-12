ca84bf7c9628f312833168c55664a350e1430ccb
Alex <lksjkltn@gmail.com>
2021-11-12 17:12:52 +0300

    make it better

--- config.js
+++ config.js
@@ -3,10 +3,12 @@ dotenv.config()
 module.exports = {
   PORT: process.env.PORT || 3200,
   accessTokenSecret: process.env.accessTokenSecret,
-  username: "fcqbbufungdofo",
-  "password": "89c158c6df69f63a1388f454af32d5a82c3f544420850784853fc8da1b9d7f0b",
-  "database": "d96a9d1j9pm6cd",
-  "host": "ec2-63-34-223-144.eu-west-1.compute.amazonaws.com:5432",
-  "dialect": "postgres"
   
+  "production": {
+    "use_env_variable":"DATABASE_URL",
+    "dialect": "postgres",
+    "dialectOptions": {
+      ssl: { rejectUnauthorized: false }
+    }
+  }
 }
